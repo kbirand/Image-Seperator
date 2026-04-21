d39e922831ed64ba1c79133772a45a6cac560f3a
Koray Birand <kbirand@mac.com>
2026-04-21 11:12:25 +0200

    Initial Commit

---
+++ Sperator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sperator
+//
+//  Created by Koray Bırand on 21.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sperator/SperatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SperatorApp.swift
+//  Sperator
+//
+//  Created by Koray Bırand on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SperatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
